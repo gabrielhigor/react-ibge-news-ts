7bc69c3eac8241def94bbd6026ce2898e50df5b7
Higor Gabriel <higor448.pessoal@gmail.com>
2023-10-16 18:17:51 -0300

    Initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,57 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es2021': true
+  },
+  'extends': [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended'
+  ],
+  'overrides': [
+    {
+      'env': {
+        'node': true
+      },
+      'files': [
+        '.eslintrc.{js,cjs}'
+      ],
+      'parserOptions': {
+        'sourceType': 'script'
+      }
+    }
+  ],
+  'parser': '@typescript-eslint/parser',
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module'
+  },
+  'settings': {
+    'react': {
+      'version': 'detect',
+    },
+  },
+  'plugins': [
+    '@typescript-eslint',
+    'react'
+  ],
+  'rules': {
+    'react/react-in-jsx-scope': 'off',
+    'indent': [
+      'error',
+      2
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'always'
+    ]
+  }
+};
